a63c3e4c1b0c3786e3258e8aee98f1a0150babb8
Benchmarkk <2200196@slu.edu.ph>
2023-03-21 02:45:25 +0800

    Changes done on HomeScreen, UserProfileScreen

--- src/screens/UserProfileScreen.js
+++ src/screens/UserProfileScreen.js
@@ -5,6 +5,7 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 import ListItem from '../components/ListItem'
 import ListImage from '../components/ListImage'
 import {plantListLibrary} from '../model/data'
+import { s } from 'react-native-wind';
 
 const UserProfileScreen = ({navigation}) => {
     const [switchView,setSwitchView] = useState(true); 
@@ -12,7 +13,7 @@ const UserProfileScreen = ({navigation}) => {
         <SafeAreaView style={{flex:1, backgroundColor:'white'}}>
             <ScrollView style={{padding:40}}>
                 <View style={{flex:1,alignItems:'center',marginBottom:20}}>
-                    <Image source={require('../assets/images/user-profile.jpg')} 
+                    <Image source={require('../assets/images/profile-icon.png')} 
                            style={{width:150,height:150, marginTop:50, borderRadius: 75}}  
                     />
                     <Text style={{fontSize:20, fontWeight:'bold', color:'#1C4C4E', marginTop:10}}>User</Text>
@@ -29,7 +30,49 @@ const UserProfileScreen = ({navigation}) => {
                         <Text>Title</Text>
                     </View>
                 </View>
-                <View style={{width: 350,padding:7,borderTopWidth:1,borderTopColor:'black', marginTop:50}} />
+                <View style={{paddingTop: 10,  flexDirection: 'row', alignContent: 'center', justifyContent: 'center'}}>
+                    <View style={{alignContent: 'center', justifyContent: 'center'}}>
+                        <Image
+                            source={require('../assets/images/rank-icon.png')}
+                            resizeMode="contain"
+                            style={{width:35,height:35}}
+                        />  
+                        <Text style={{fontSize: 12}}>Bronze</Text> 
+                    </View>   
+                    <View style={{marginLeft:15, alignContent: 'center', justifyContent: 'center'}}>      
+                        <Image
+                            source={require('../assets/images/empty-rank-icon.png')}
+                            resizeMode="contain"
+                            style={{width:35,height:35}}
+                        />  
+                        <Text style={{marginLeft: 3, fontSize: 12}}>Silver</Text> 
+                    </View>      
+                    <View style={{marginLeft:15, alignContent: 'center', justifyContent: 'center'}}>      
+                        <Image
+                            source={require('../assets/images/empty-rank-icon.png')}
+                            resizeMode="contain"
+                            style={{width:35,height:35}}
+                        />  
+                        <Text style={{marginLeft: 4, fontSize: 12}}>Gold</Text> 
+                    </View>  
+                    <View style={{marginLeft:15, alignContent: 'center', justifyContent: 'center'}}>      
+                        <Image
+                            source={require('../assets/images/empty-rank-icon.png')}
+                            resizeMode="contain"
+                            style={{width:35,height:35}}
+                        />  
+                        <Text style={{marginLeft: -6,fontSize: 12}}>Diamond</Text> 
+                    </View> 
+                    <View style={{marginLeft:15, alignContent: 'center', justifyContent: 'center'}}>      
+                        <Image
+                            source={require('../assets/images/empty-rank-icon.png')}
+                            resizeMode="contain"
+                            style={{width:35,height:35}}
+                        />  
+                        <Text style={{marginLeft: -4, fontSize: 12}}>Botanist</Text> 
+                    </View>  
+                </View>
+                <View style={{width: 350,padding:7,borderTopWidth:1,borderTopColor:'black', marginTop:30}} />
                 <View style={{flexDirection:'row',alignItems:'center', justifyContent: 'center'}}>             
                     <View style={{borderRightWidth: 1, 
                                   borderColor:'black',
@@ -68,8 +111,8 @@ const UserProfileScreen = ({navigation}) => {
                         }
                     </View>
                 ) : (
-                    <View>
-                        <Text>Achievements</Text>
+                    <View style={{alignItems:'center', justifyContent: 'center'}}>
+                        <Text>Complete Missions to get Achievements</Text>
                     </View>
                 )}
             </ScrollView>
