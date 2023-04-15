0f721a29f5853bd6cee8005787b52afbb3584b05
KarylleD <2200161@slu.edu.ph>
2023-04-15 23:17:34 +0800

    Edits on Data

--- src/model/IndigenousPlantsLocation.js
+++ src/model/IndigenousPlantsLocation.js
@@ -1,26 +1,30 @@
 export const PlantsLocation = [
     {
+        id: 1,
         type: 'Medicinal',
         name: 'Blumea balsamifera',
-        latitude: '16.3005',
-        longitude:'120.5740'
+        latitude: 16.3005,
+        longitude: 120.5740
+    },
+    {
+        id: 2,
+        type: "Medicinal",
+        name: "Ehretia microphylla",
+        latitude: 16.3452,
+        longitude:120.6890
+    },
+    {
+        id: 3,
+        type: "Medicinal,Food",
+        name: "Psidium guajav",
+        latitude: 16.4885,
+        longitude:120.5164
+    },
+    {
+        id: 4,
+        type: "Medicinal",
+        name: "Vitex negundo",
+        latitude: 16.6170,
+        longitude:120.8269
     }
-   /* {
-        "type": "Medicinal",
-        "name": "Ehretia microphylla",
-        "latitude": "16.3452",
-        "longitude":"120.6890"
-    },
-    {
-        "type": "Medicinal,Food",
-        "name": "Psidium guajav",
-        "latitude": "16.4885",
-        "longitude":"120.5164"
-    },
-    {
-        "type": "Medicinal",
-        "name": "Vitex negundo",
-        "latitude": "16.6170",
-        "longitude":"120.8269"
-    }*/
 ]
