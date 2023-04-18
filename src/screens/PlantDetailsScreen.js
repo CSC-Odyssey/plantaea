1a41cc94794d5d93ce82edc307a44d8b6d392d7a
Shirorinku <linkgen42070@gmail.com>
2023-04-18 12:57:46 +0800

    edits on data

--- src/screens/PlantDetailsScreen.js
+++ src/screens/PlantDetailsScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { View, Text, Image, SafeAreaView, ScrollView, ImageBackground } from "react-native";
-import { windowWidth } from '../utils/Dimensions'
+import { windowWidth, windowHeight } from '../utils/Dimensions'
 
 import Feather from 'react-native-vector-icons/Feather'
 import { TouchableOpacity } from "react-native-gesture-handler";
@@ -15,12 +15,17 @@ const PlantDetailsScreen = ({navigation, route}) => {
 
     return(
         <SafeAreaView style={{flex:1, backgroundColor:'white'}}>
-        <ScrollView style={{padding:40}}>
-        <TouchableOpacity onPress={()=>navigation.goBack()} style={{flexDirection:'row', marginBottom:20}}>
+            <View>
+
+                <ImageBackground source={route.params?.image} style={{width:windowWidth ,height:400}} imageStyle={{borderRadius:0}}/>
+            </View>
+
+<ScrollView style={{padding:20}}>
+<TouchableOpacity onPress={()=>navigation.goBack()} style={{flexDirection:'row', marginBottom:20}}>
             <Feather name="chevron-left" color="#1C4C4E" size={30}/>
         </TouchableOpacity>
+
         <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-        <ImageBackground source={route.params?.image} style={{width:300,height:200}} imageStyle={{borderRadius:20}}/>
         <Text style={{fontWeight:'bold', fontSize:30, color:'#1C4C4E'}}>{route.params?.localName}</Text>
         <Text style={{fontStyle:'italic', marginBottom:20}}>{route.params?.scientificName}</Text>
 
@@ -36,7 +41,7 @@ const PlantDetailsScreen = ({navigation, route}) => {
 
         {descriptionTab == 1 && <Text style={{textAlign:'justify', marginBottom:100}}>{route.params?.description}</Text>}
         {descriptionTab == 2 && <Text style={{textAlign:'justify', marginBottom:100}}>{route.params?.use}</Text>}
-        {descriptionTab == 3 && <Text>Taxonomy</Text>}
+        {descriptionTab == 3 && <Text style={{textAlign:'justify', marginBottom:100, fontSize:10}}>{route.params?.taxonomy}</Text>}
 
         
         </View>
